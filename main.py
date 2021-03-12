3c71d8b202f8ed8fce3fb19dae1725abf32de7e4
asamilyk <asamilyk@yandex.ru>
2021-03-12 18:15:13 +0300

    color random circle go to wall

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pygame
+import random
+
 
 class Ball():
-    def __init__(self, pas):
-        self.x = pas[0]
-        self.y = pas[1]
-        self.color = (255, 0, 0)
+    def __init__(self):
+        self.x = random.randint(100, 500)
+        self.y = random.randint(100, 500)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.r = 50
-        self.step_x = 0
-        self.step_y = 0
+        self.step_x = random.randint(-5, 5)
+        self.step_y = random.randint(-5, 5)
 
     def move(self):
         self.x += self.step_x
@@ -21,17 +23,12 @@ class Ball():
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
 
 
-
-
-
 FPS = 250
 clock = pygame.time.Clock()
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 running = True
-first_ball = Ball((100, 100))
-first_ball.change_dir(1,1)
-
+list_ball = [Ball() for i in range(10)]
 
 while running:
     for event in pygame.event.get():
@@ -39,22 +36,19 @@ while running:
             running = False
 
     screen.fill((0, 0, 0))
-    first_ball.update()
-    first_ball.move()
-    if first_ball.x + first_ball.r == 800:
-        first_ball.change_dir(-first_ball.step_x,first_ball.step_y)
-    elif first_ball.x - first_ball.r == 0:
-        first_ball.change_dir(-first_ball.step_x,first_ball.step_y)
-    if first_ball.y + first_ball.r == 600:
-        first_ball.change_dir(first_ball.step_x,-first_ball.step_y)
-    elif first_ball.y - first_ball.r == 0:
-        first_ball.change_dir(first_ball.step_x,-first_ball.step_y)
-
-
-
+    for bal in list_ball:
+        bal.update()
+        bal.move()
+        if bal.x + bal.r >= 800:
+            bal.change_dir(-bal.step_x, bal.step_y)
+        elif bal.x - bal.r <= 0:
+            bal.change_dir(-bal.step_x, bal.step_y)
+        if bal.y + bal.r >= 600:
+            bal.change_dir(bal.step_x, -bal.step_y)
+        elif bal.y - bal.r <= 0:
+            bal.change_dir(bal.step_x, -bal.step_y)
 
     clock.tick(FPS)
     pygame.display.flip()
-
 
 pygame.quit()
