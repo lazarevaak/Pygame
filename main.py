2507cfc6880fc7695c526c57410949b7291d83b4
asamilyk <asamilyk@yandex.ru>
2021-03-12 16:34:56 +0300

    creat window

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+rinning = True
+while rinning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            rinning = False
+    pass
+
+pygame.quit()
